13ce8f5b8c1073b469fc4d3b7bfcb53372d4ed7d
Michele Ambrosio <micheleambrosio.dev@gmail.com>
2024-01-31 20:25:51 -0300

    Home page form for create or join in a meet

---
+++ src/components/MeetingFormTabs/MeetForm/index.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import * as Form from "@radix-ui/react-form";
+
+interface Props {
+  type: "create-meet" | "join-meet";
+}
+
+export default function NewMeetForm({ type }: Props) {
+  return (
+    <Form.Root className="flex flex-col gap-5">
+      <Form.Field name="name">
+        <div className="flex justify-between">
+          <Form.Label className="text-sm text-gray-800 mb-1">
+            Your name
+          </Form.Label>
+          <Form.Message
+            match="valueMissing"
+            className="text-[13px] text-gray-600 opacity-[0.8]"
+          >
+            Please enter your name
+          </Form.Message>
+        </div>
+        <Form.Control asChild>
+          <input
+            type="text"
+            required
+            className="w-full bg-blackA2 shadow-gray-400 h-[35px] rounded-[4px] px-[10px] text-[15px] leading-none text-gray-800 shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_purple] focus:shadow-[0_0_0_2px_black]"
+          />
+        </Form.Control>
+      </Form.Field>
+      {type === "join-meet" && (
+        <Form.Field name="id">
+          <div className="flex justify-between">
+            <Form.Label className="text-sm text-gray-800 mb-1">
+              Meeting ID
+            </Form.Label>
+            <Form.Message
+              match="valueMissing"
+              className="text-[13px] text-gray-600 opacity-[0.8]"
+            >
+              Please enter meeting id
+            </Form.Message>
+          </div>
+          <Form.Control asChild>
+            <input
+              type="text"
+              required
+              className="w-full bg-blackA2 shadow-gray-400 h-[35px] rounded-[4px] px-[10px] text-[15px] leading-none text-gray-800 shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_purple] focus:shadow-[0_0_0_2px_black]"
+            />
+          </Form.Control>
+        </Form.Field>
+      )}
+      <Form.Submit asChild>
+        <button className="w-full p-2 bg-purple-700 hover:bg-purple-500 rounded-xl text-white focus:shadow-[0_0_0_2px_black] outline-none">
+          {type === "create-meet" ? "Create Meet" : "Join Meet"}
+        </button>
+      </Form.Submit>
+    </Form.Root>
+  );
+}
---
+++ src/components/MeetingFormTabs/index.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import * as Tabs from "@radix-ui/react-tabs";
+import MeetForm from "./MeetForm";
+
+export default function MeetingFormTabs() {
+  return (
+    <section className="w-4/6 h-full flex flex-col justify-center items-center">
+      <h1 className="text-4xl mb-5 font-bold text-gray-800 lowercase">
+        <div className="text-3xl text-center">💬</div>
+        Talk to Me
+      </h1>
+      <Tabs.Root
+        defaultValue="join-meet"
+        className="flex w-3/6 flex-col bg-white rounded-xl"
+      >
+        <Tabs.List
+          aria-label="Join an existing meet or create one"
+          className="flex flex-shrink-0 border-b"
+        >
+          <Tabs.Trigger
+            value="join-meet"
+            className="p-5 flex-1 flex items-center justify-center text-gray-800 data-[state=active]:text-purple-700 hover:text-purple-500 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current"
+          >
+            Join
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            value="new-meet"
+            className="p-5 flex-1 flex items-center justify-center text-gray-800 data-[state=active]:text-purple-700 hover:text-stone-500 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current"
+          >
+            New Meeting
+          </Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="join-meet" className="p-5">
+          <MeetForm type="join-meet" />
+        </Tabs.Content>
+        <Tabs.Content value="new-meet" className="p-5">
+          <MeetForm type="create-meet" />
+        </Tabs.Content>
+      </Tabs.Root>
+    </section>
+  );
+}
